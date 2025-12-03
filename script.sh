a39eb66fb5a362257f207dc6fd615b5945e89e10
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2025-12-03 15:41:47 +0000

    Guardado1

---
+++ script.sh
@@ -0,0 +1,6 @@
+#Crea y listar archivos
+echo "Hola Linux" > archivo.txt #Para mostrar un mensaje de varias líneas sin mostrar ningún comando
+cat archivo.txt #te permite crear, fusionar o imprimir archivos en la pantalla de salida estándar o en otro archivo
+cp archivo.txt copia.txt #copiar uno o más archivos a una ubicación especificada por el usuario
+ls -l #El comando ls lista archivos y directorios en sistemas operativos Linux y similares a Unix
+rm archivo.txt #se utiliza para borrar archivos y directorios de forma completa y sin posibilidad de ser recuperados
