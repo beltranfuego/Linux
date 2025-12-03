5bb0d4b41b6fda3c1f37272b81f54ff966e09566
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2025-12-03 15:55:06 +0000

    guardado final

--- script.sh
+++ script.sh
@@ -3,4 +3,5 @@ echo "Hola Linux" > archivo.txt #Para mostrar un mensaje de varias líneas sin m
 cat archivo.txt #te permite crear, fusionar o imprimir archivos en la pantalla de salida estándar o en otro archivo
 cp archivo.txt copia.txt #copiar uno o más archivos a una ubicación especificada por el usuario
 ls -l #El comando ls lista archivos y directorios en sistemas operativos Linux y similares a Unix
+# -l long format, proporciona masssss información
 rm archivo.txt #se utiliza para borrar archivos y directorios de forma completa y sin posibilidad de ser recuperados
--- script2.sh
+++ script2.sh
@@ -1,5 +1,8 @@
 uname -a #se usa para imprimir información básica del sistema.
+# -a solicita la informacion disponible como el nombre del nodo, version del kernel, fecha de compilacion del kernel, 
+# arquitectura de la maquina y el SO
 lscpu # proporciona una lista detallada de información sobre las CPU del sistema
 free -h # permite verificar la memoria RAM en su sistema o verificar las estadísticas de memoria del sistema
+#-h human readable muestra unidades mas comprensibles como KB, MG, GB en vez bytes
 df -h #nos informa acerca del espacio total, ocupado y libre en nuestro sistema
 
