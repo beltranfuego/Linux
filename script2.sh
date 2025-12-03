ec52270790f28a3c764238229a4833a6a66defeb
ARTHUR BELTRAN <arbeltranhe@uide.edu.ec>
2025-12-03 15:46:02 +0000

    guardadito 2

--- script2.sh
+++ script2.sh
@@ -1,4 +1,5 @@
-uname -a
-lscpu
-
+uname -a #se usa para imprimir información básica del sistema.
+lscpu # proporciona una lista detallada de información sobre las CPU del sistema
+free -h # permite verificar la memoria RAM en su sistema o verificar las estadísticas de memoria del sistema
+df -h #nos informa acerca del espacio total, ocupado y libre en nuestro sistema
 
